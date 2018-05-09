ecc41ef07d4cecf1e27fd59e4b3c949f74ee686d
Toby Huang <619058900@qq.com>
2018-05-09 18:31:24 +1000

    Try to move and restrict the block.

--- src/game/Car.java
+++ src/game/Car.java
@@ -1,8 +1,6 @@
 package game;
 
-import javafx.geometry.Pos;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
@@ -70,19 +68,48 @@ public class Car extends StackPane {
         
         //set the function for mouse clicking on
         setOnMousePressed(e -> {
-        	this.mouseX = e.getLayoutX();
-        	this.mouseY = e.getLayoutY();
-//        	this.mouseX = e.getSceneX();
-//        	this.mouseY = e.getSceneY();        	
+        	this.mouseX = e.getSceneX()-gridX*GameController.GRID_SIZE;
+        	this.mouseY = e.getSceneY()-gridY*GameController.GRID_SIZE;
         });
         
         //set the function for mouse dragging
         setOnMouseDragged(e -> {
-        	relocate(e.getSceneX() - mouseX + oldX, e.getSceneY() - mouseY + oldY);
+            int mouseGridX, mouseGridY;
+            // calculate the suppose grid x and grid y
+            // by mouse position
+            mouseGridX = (int )(e.getSceneX() / GameController.GRID_SIZE);
+            mouseGridY = (int )(e.getSceneY() / GameController.GRID_SIZE);
+
+            System.out.println("Axis: "+ mouseGridX +
+                    " isCollision " + isCollision(mouseGridX,this.gridY));
+
+            if(this.dir == MoveDir.HORIZONTAL){
+                if (!isCollision(mouseGridX,this.gridY)){
+                    // no collision, relocate the block
+                    relocate(e.getSceneX() - this.mouseX,
+                            this.gridY * GameController.GRID_SIZE);
+                }
+            }
+            else{
+                if (!isCollision(this.gridX,mouseGridY)){
+                    // no collision, relocate the block
+                    relocate(this.gridX * GameController.GRID_SIZE,
+                            e.getSceneY() - this.mouseY);
+                }
+            }
+
+
         });
 
     }
 
+    public boolean isCollision(int gridX, int gridY){
+        if (gridX <= 0  || gridX + this.getLen()>6 ||
+            gridY <= 0  || gridY+ this.getLen()>6) {
+            return true;
+        }
+        return false;
+    }
 
     public int getCarId() {
         return carId;
@@ -119,6 +146,9 @@ public class Car extends StackPane {
     }
     
     public void move (int gridX, int gridY) {
+        //limit
+
+
     	oldX = gridX * GameController.GRID_SIZE;
     	oldY = gridY * GameController.GRID_SIZE;
     	relocate(oldX, oldY);    	
