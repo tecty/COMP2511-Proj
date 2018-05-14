20bae3be9da4d9c8f7ac7db3b78b7cccd3672cfb
zzzsoftware <30430450+zzzsoftware@users.noreply.github.com>
2018-05-14 12:47:32 +1000

    added moveCar and undo

--- src/Board.java
+++ src/Board.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 class Board {
 	public int [][] Board;
 	public ArrayList<Car> Car;
+	//移动车子的顺序，存车子的id
+	public ArrayList<Integer> carID;
 	
 	
 	
@@ -13,10 +15,11 @@ class Board {
 			}
 		}
 		this.Car = c;
+		this.carID = new ArrayList<Integer>();
 	}
 	
 	
-	
+	//for debug
 	public static void printB(Board in) {
 		for(int i = 0;i < 6;i ++) {
 			for(int j = 0;j < 6; j ++) {
@@ -37,6 +40,10 @@ class Board {
 					
 		}
 	}
+	
+	
+	
+	
 	
 	public static void main(String[] args){
 	      
@@ -68,36 +75,154 @@ class Board {
 	      System.out.println("\n\nTest to move car0 to  (2,3), (2,4) ");
 	      Coordinate test0 = new Coordinate(2,3,2,4);
 	      initial.Car.get(0).Paths.add(test0);
-	      System.out.println(isValidMove(initial.Board, initial.Car.get(0)));
+	      System.out.println(initial.isValidMove(initial.Board, initial.Car.get(0)));
 	      
 	      System.out.println("\n\nTest to move car1 to  (3,3), (5,3) ");
 	      Coordinate test1 = new Coordinate(3,3,5,3);
 	      initial.Car.get(1).Paths.add(test1);
-	      System.out.println(isValidMove(initial.Board, initial.Car.get(1)));
+	      System.out.println(initial.isValidMove(initial.Board, initial.Car.get(1)));
+	      
+	      //sample for adding  Car1 
+	      //如果只是移动了car 1 而且 car 1 的move 是允许的情况下，加入carNum
+	      if(initial.isValidMove(initial.Board, initial.Car.get(1))) {
+	    	  	  initial.moveCar(1);
+	      }
+	      
+	      printB(initial);
+	      
+	      
+	      
+	      System.out.println("\n\nTest to move car0 to  (2,3), (2,4) ");
+	      Coordinate test01 = new Coordinate(2,3,2,4);
+	      initial.Car.get(0).Paths.add(test01);
+	      System.out.println(initial.isValidMove(initial.Board, initial.Car.get(0)));
+	      if(initial.isValidMove(initial.Board, initial.Car.get(0))) {
+	    	  	  initial.moveCar(0);
+	      }
+	      
+	      printB(initial);
+	      
+	      
+	      
+	      //undo last move
+	      System.out.println("\n\n Test undo last move ");
+	      initial.undo();
+	      printB(initial);
+	      
+	      //undo last move
+	      System.out.println("\n\n Test undo last move ");
+	      initial.undo();
+	      printB(initial);
 	}
 	
-	public static boolean isValidMove(int [][] b, Car c) {
-		boolean isValid = false;
+	public boolean undo() {
+		//cannot undo at the stat of game
+		if ( this.carID.size() == 0) {
+			return false;
+		}
+		int cID = this.carID.get( this.carID.size() -1 );
+		
+		Car c = this.Car.get(cID);
+		Coordinate co = c.Paths.get( c.Paths.size() -1 );
+		Coordinate preCo = c.Paths.get( c.Paths.size() -2 );
+		
+		//move by row
+		if(co.x1 == co.x2) {
+			//reset previous
+			for(int i = co.y1 ; i <= co.y2; i ++) {
+				this.Board[co.x1][i] = -1;	
+			}
+			
+			//add new
+			for(int i = preCo.y1 ; i <= preCo.y2; i ++) {
+				this.Board[preCo.x1][i] = cID;
+			}
+			
+		}else if(co.y1 == co.y2) {
+			//reset previous
+			for(int i = co.x1; i <= co.x2; i ++) {
+				this.Board[i][co.y1] = -1;
+			}
+			
+			//add new
+			for(int i = preCo.x1; i <= preCo.x2; i ++) {
+				this.Board[i][preCo.y1] = cID;
+			}
+			
+		}
+		
+		//remove form paths
+		c.Paths.remove(c.Paths.size() -1 );
+		//remove from carID
+		this.carID.remove(this.carID.size() -1 );
+		
+		return true;
+	}
+	
+	// add cID to carID list, reset previous board, move/add new board
+	public void moveCar(int cID) {
+		//add to carID list
+		this.carID.add( cID);
+		
+		Car c = this.Car.get(cID);
+		Coordinate co = c.Paths.get( c.Paths.size() -1 );
+		Coordinate preCo = c.Paths.get( c.Paths.size() -2 );
+		
+		//move by row
+		if(co.x1 == co.x2) {
+			//reset previous
+			for(int i = preCo.y1 ; i <= preCo.y2; i ++) {
+				this.Board[preCo.x1][i] = -1;
+			}
+			
+			//add new
+			for(int i = co.y1 ; i <= co.y2; i ++) {
+				this.Board[co.x1][i] = cID;	
+			}
+			
+		}else if(co.y1 == co.y2) {
+			//reset previous
+			for(int i = preCo.x1; i <= preCo.x2; i ++) {
+				this.Board[i][preCo.y1] = -1;
+			}
+			//add new
+			for(int i = co.x1; i <= co.x2; i ++) {
+				this.Board[i][co.y1] = cID;
+			}
+		}
+		
+	}
+	
+	
+	
+	public  boolean isValidMove(int [][] b, Car c) {
+		//boolean isValid = false;
 		Coordinate co = c.Paths.get(c.Paths.size()-1);
 		
 		if(c.num == 0 && (co.y1 > 5 || co.y2 > 5)) {
+			//congratulations
 			return true;
 		}
 		
 		//out of range
-		if(co.x1 > 5 || co.x2 > 5 || co.y2 > 5 || co.y2 > 5)
+		if(co.x1 > 5 || co.x2 > 5 || co.y2 > 5 || co.y2 > 5 ||
+		 co.x1 < 0 || co.x2 < 0  || co.y2 < 0  || co.y2 < 0) {
+			c.Paths.remove(  c.Paths.size()-1 );
 			return false;
-
+		}
+		
 		//move by row
 		if(co.x1 == co.x2) {
 			for(int i = co.y1 ; i <= co.y2; i ++) {
-				if(b[co.x1][i] != -1) {
+				if(b[co.x1][i] != -1 && b[co.x1][i] != c.num) {
+					c.Paths.remove(  c.Paths.size()-1 );
 					return false;
 				}
 			}
 		}else if(co.y1 == co.y2) {
 			for(int i = co.x1; i <= co.x2; i ++) {
-				if(b[i][co.y1] != -1) {
+				if(b[i][co.y1] != -1 && b[i][co.y1] != c.num) {
+					c.Paths.remove(  c.Paths.size()-1 );
 					return false;
 				}
 			}
