cb6f46e061a4c68bf2e280f7caefc44f99057ccd
Maggie_AKI <maggie.zhu2016@outlook.com>
2018-05-21 09:44:43 +1000

    complete credit output

--- src/setting/Bgm.java
+++ src/setting/Bgm.java
@@ -7,12 +7,33 @@ import javafx.util.Duration;
 public class Bgm {
 	//mediaPlayer-object will not we cleaned away since someone holds a reference to it!	
 	private static MediaPlayer mediaPlayer;
+	private static boolean playable = true;
 
+	public static void changeStat(boolean result) {
+		playable = result;
+		if(!result) {
+			mediaPlayer.stop();
+		}
+		else {
+			Media hit = new Media(Paths.get("music/bgm.mp3").toUri().toString());
+		    mediaPlayer = new MediaPlayer(hit);
+			mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
+		    mediaPlayer.setAutoPlay(true);
+		}
+	}
+	
+	public static boolean getStat() {
+		return playable;
+	}
+	
 	public static void play()
 	{
-		Media hit = new Media(Paths.get("music/bgm.mp3").toUri().toString());
-	    mediaPlayer = new MediaPlayer(hit);
-	    mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
-	    mediaPlayer.setAutoPlay(true);
+		if(playable) {
+			Media hit = new Media(Paths.get("music/bgm.mp3").toUri().toString());
+		    mediaPlayer = new MediaPlayer(hit);
+		    mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
+		    mediaPlayer.setAutoPlay(true);
+		}
+		else return;
 	}
 }
--- src/setting/SoundEffect.java
+++ src/setting/SoundEffect.java
@@ -6,12 +6,24 @@ import javafx.scene.media.*;
 
 public class SoundEffect{
 	private static MediaPlayer mediaPlayer;
+	private static boolean playable = true;
+	
+	public static void changeStat(boolean result) {
+		playable = result;
+	}
+	
+	public static boolean getStat() {
+		return playable;
+	}
 	
 	public static void play(String fileName) {
-		// TODO Auto-generated method stub
-		Media hit = new Media(Paths.get(fileName).toUri().toString());
-		mediaPlayer = new MediaPlayer(hit);
-		mediaPlayer.play();
+		if(playable) {
+			// TODO Auto-generated method stub
+			Media hit = new Media(Paths.get(fileName).toUri().toString());
+			mediaPlayer = new MediaPlayer(hit);
+			mediaPlayer.play();
+		}
+		else return;
 	}
 
 }
