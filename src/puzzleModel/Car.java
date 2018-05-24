035bd4a896e2f5e7d787761099bb1ddc322cc5ee
zzzsoftware <30430450+zzzsoftware@users.noreply.github.com>
2018-05-24 22:02:03 +1000

    added javadoc

--- src/puzzleModel/Car.java
+++ src/puzzleModel/Car.java
@@ -1,37 +1,54 @@
 package puzzleModel;
 
 import java.util.ArrayList;
-
-public class Car {
-    //public ArrayList<Coordinate> Paths = new ArrayList<Coordinate>();
+/**
+ * Car consisting of carID and Path of the car
+ * @author Huiyue Zhang, Ziqing Yan
+ *
+ */
+class Car {
+	//record car ID of a car (should be all unique)
     public int carID;
+    //record the path of how the car moved
     public ArrayList<Coordinate> Paths;
-
+    
+    /**
+     * Constructor
+     * @precondition all param is valid 
+     * @param num
+     * @param Paths
+     */
     public Car(int num, ArrayList<Coordinate> Paths) {
         this.carID = num;
         this.Paths = Paths;
     }
-
+    
+    
+    /**
+     * get function for a copy of the coordinate
+     * @return new Car
+     */
     public Car getCopy() {
+    		//creates new list and add copy of all coordinate
         ArrayList<Coordinate> p = new ArrayList<Coordinate>();
         for (int i = 0; i < this.Paths.size(); i++) {
             p.add(this.Paths.get(i).getCopy());
         }
+        //creates new car
         Car c = new Car(this.carID, p);
         return c;
     }
 
-
+    /**
+     * override hashCode method for Car
+     */
     @Override
     public int hashCode() {
-        //System.out.println("CALLING HashCar");
         int hash = 1;
         Coordinate co = this.Paths.get(this.Paths.size() - 1);
         hash = hash * 2 + this.carID;
-
         // hash horizontal or vertical
         hash = hash * 3 + ((co.x1 == co.x2) ? 0 : 1);
-
         // hash length
         if (co.x1 == co.x2) {
             hash = hash * 5 + co.y2 - co.y1 + 1;
@@ -42,11 +59,13 @@ public class Car {
         hash = hash * 11 + co.y1;
         return hash;
     }
-
+    
+    
+    /**
+     * override equals method for Car
+     */
     @Override
     public boolean equals(Object o) {
-        //System.out.println("CALLING EqualsCar");
-
         if (o == this) return true;
 
         if (o == null || !(o instanceof Car)) return false;
@@ -74,16 +93,23 @@ public class Car {
         return true;
     }
 
+    
+    	/**
+    	 * get method for carID
+    	 * @return carID
+    	 */
     public int getCarID() {
         return carID;
     }
 
+    
+    	/**
+    	 * get  method of Path size
+    	 * @return Paths.size()
+    	 */
     public int getPathSize() {
-//		return the path size to check whether it has been used
+    		//return the path size to check whether it has been used
         return Paths.size();
     }
 
-    public Coordinate popFirstCo() {
-        return Paths.get(0);
-    }
 }
--- src/puzzleModel/Coordinate.java
+++ src/puzzleModel/Coordinate.java
@@ -1,18 +1,35 @@
 package puzzleModel;
-
-public class Coordinate {
+/**
+ * Coordinate consisting of coordinates 
+ * of a car : (x1,y1) and (x2,y2)
+ * @author Ziqing Yan, Huiyue Zhang
+ *
+ */
+class Coordinate {
     public int x1;
     public int y1;
     public int x2;
     public int y2;
-
+    
+    /**
+     * Constructor 
+     * @precondition all param is valid 
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     */
     public Coordinate(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
     }
-
+    
+    /**
+     * get function for a copy of the coordinate
+     * @return new Coordinate
+     */
     public Coordinate getCopy() {
         return new Coordinate(this.x1, this.y1, this.x2, this.y2);
     }
